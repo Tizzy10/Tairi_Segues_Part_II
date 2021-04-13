d941da2a8994e4c6a22cba19ac6acfcb4403a657
TizzJohnson <tizznasty10@gmail.com>
2021-04-13 11:17:46 -0400

    Initial Commit

---
+++ Tairi_Segues_Part_II/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tairi_Segues_Part_II
+//
+//  Created by Tairi on 4/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
