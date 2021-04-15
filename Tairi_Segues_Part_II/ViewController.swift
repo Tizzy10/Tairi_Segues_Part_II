6c6e6861b0aac95557c691b5a7be0b73b3454b3d
TizzJohnson <tizznasty10@gmail.com>
2021-04-15 13:25:22 -0400

    Completed Segues P2 Lab

--- Tairi_Segues_Part_II/ViewController.swift
+++ Tairi_Segues_Part_II/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var segueSwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    /*override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+            return segueSwitch.isOn
+        }*/
+    
+    @IBAction func goToBlue(_ sender: Any) {
+        if segueSwitch.isOn {
+        performSegue(withIdentifier: "Blue", sender: nil)
+        }
+    }
+    
+    @IBAction func goToOrange(_ sender: Any) {
+        if segueSwitch.isOn {
+        performSegue(withIdentifier: "Orange", sender: nil)
+        }
+    }
 }
 
